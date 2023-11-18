3100cc2513bbac1f5fb12083a833452e5e15c1c5
kavi khna <khankavi21@gmail.com>
2023-11-18 19:22:57 +0530

    Till express async handler

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+const express = require("express")
+const dotenv = require('dotenv').config()
+const {errorHandler}= require('./middleware/errorMiddleware.js')
+const port = process.env.PORT || 5000 
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false }))
+
+app.get("/api/goals",(req,res)=>{
+//    res.send('Get Goals');
+   res.json({message : 'Get Goals'})
+})
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+app.use(errorHandler)
+
+app.listen(port , ()=>{
+    console.log(`Server started on port ${port}`)
+})
